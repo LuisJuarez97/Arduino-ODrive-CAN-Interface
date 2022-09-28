9d85bc1166cdb8e9592c9cebd466a4db4762dcfc
Luis Juarez <luisalexanderjm@hotmail.com>
2022-09-27 20:25:29 -0500

    Receive and decode messages from ODrive. Show node id and cmd id

--- CAN_ODrive/CAN_ODrive.ino
+++ CAN_ODrive/CAN_ODrive.ino
@@ -1,3 +1,4 @@
+#include <LibPrintf.h>
 #include "CAN_Node.h"
 
 CAN_Node Axis[] = { AXIS0_NODE_ID, AXIS1_NODE_ID };
@@ -12,6 +13,8 @@ void setup() {
   if (!CAN.begin(CAN_BAUDRATE)) {
     Serial.println("Starting CAN failed!");
     while (1);
+  } else {
+    Serial.println("CAN Running!");
   }
 }
 
@@ -20,10 +23,25 @@ unsigned long long offset = 0;
 int Tm = 1;
 int A = 50;
 int S = 5;
-bool disable;
+bool wave_enable = false;
 
 void loop() {
-  if (!disable) {
+  waveRutine();
+
+
+  if (Serial.available()){
+    char cmd = Serial.read();
+    modifyBehaviourWave(cmd);
+    sendMessagesToODrive(cmd);
+  }
+  readMessagesFromODrive();
+}
+
+
+
+
+void waveRutine(void) {
+  if (wave_enable) {
     if ((millis() - prevMillis) > Tm) {
       prevMillis = millis();
       unsigned long long x = prevMillis - offset;
@@ -32,63 +50,100 @@ void loop() {
       Axis[1].setInputPos(A*sin(x*M_PI/(S*1000)));
     }
   }
+}
 
-
-  if (Serial.available()){
-    char cmd = Serial.read();
-    if (cmd == 'A') {
-      A = Serial.parseInt();
-    }
-    if (cmd == 'S') {
-      S = Serial.parseInt();
-    }
-    if (cmd == 'D') {
-      disable = Serial.parseInt();
-    }
-
-
-
-    if (cmd == 'a') {
-      current_axis = Serial.parseInt();
-      Serial.write("Axis : ");
-      Serial.println(current_axis);
-    }
-    if (cmd == 'f') {
-      Serial.write("Requesting AXIS_STATE_FULL_CALIBRATION_SEQUENCE in Axis ");
-      Serial.println(current_axis);
-      Axis[current_axis].sendCommand(SET_AXIS_REQUESTED_STATE, AXIS_STATE_FULL_CALIBRATION_SEQUENCE);
-    }
-    if (cmd == 'c') {
-      Serial.write("Requesting AXIS_STATE_CLOSED_LOOP_CONTROL in Axis ");
-      Serial.println(current_axis);
-      Axis[current_axis].setAxisRequestedState(AXIS_STATE_CLOSED_LOOP_CONTROL);
-    }
-    if (cmd == 'i') {
-      Serial.write("Requesting AXIS_STATE_IDLE in Axis ");
-      Serial.println(current_axis);
-      Axis[current_axis].setAxisRequestedState(AXIS_STATE_IDLE);
-    }
-    if (cmd == 'p') {
-      int pos = Serial.parseInt();
-      Serial.write("input position:\t");
-      Serial.println(pos);
-      Axis[current_axis].setInputPos(pos);
-    }
-    if (cmd == '?') {
-      Serial.write("Info    :\n");
-      Serial.write("\tCurrent_axis:\t");  Serial.println(current_axis);
-      Serial.write("\tNode_id:\t");  Serial.println(Axis[current_axis].getNodeId());
-    }
+void modifyBehaviourWave(char cmd) {
+  if (cmd == 'A') {
+    A = Serial.parseInt();
   }
+  if (cmd == 'S') {
+    S = Serial.parseInt();
+  }
+  if (cmd == 'E') {
+    wave_enable = Serial.parseInt();
+  }
+}
 
-  /*int packetSize = CAN.parsePacket();
-  Serial.println(packetSize);
+void sendMessagesToODrive(char cmd) {
+  if (cmd == 'a') {
+    current_axis = Serial.parseInt();
+    printf("Axis : %d\n", current_axis);
+  }
+  if (cmd == 'f') {
+    printf("Requesting AXIS_STATE_FULL_CALIBRATION_SEQUENCE in Axis %d\n", current_axis);
+    Axis[current_axis].sendCommand(SET_AXIS_REQUESTED_STATE, AXIS_STATE_FULL_CALIBRATION_SEQUENCE);
+  }
+  if (cmd == 'c') {
+    printf("Requesting AXIS_STATE_CLOSED_LOOP_CONTROL in Axis %d\n", current_axis);
+    Axis[current_axis].setAxisRequestedState(AXIS_STATE_CLOSED_LOOP_CONTROL);
+  }
+  if (cmd == 'i') {
+    printf("Requesting AXIS_STATE_IDLE in Axis %d\n", current_axis);
+    Axis[current_axis].setAxisRequestedState(AXIS_STATE_IDLE);
+  }
+  if (cmd == 'p') {
+    int pos = Serial.parseInt();
+    printf("input position:\t%d\n", pos);
+    Axis[current_axis].setInputPos(pos);
+  }
+  if (cmd == '?') {
+    printf("Info:\n");
+    printf("\tCurrent_axis:\t%d\n", current_axis);
+    printf("\tNode_id:\t%d\n", Axis[current_axis].getNodeId());
+  }
+}
 
+void readMessagesFromODrive(void) {
+  int packetSize = CAN.parsePacket();
   if (packetSize) {
-    Serial.print("packet with id 0x");
-    Serial.print(CAN.packetId(), HEX);
-    while (CAN.available()) {
-      //Serial.print(CAN.read());
+    Serial.print("Received ");
+    if (CAN.packetExtended()) {
+      Serial.print("extended ");
     }
-  }*/
+    if (CAN.packetRtr()) {
+      // Remote transmission request, packet contains no data
+      Serial.print("RTR ");
+    }
+    short packetId = CAN.packetId();
+    byte nodeId = packetId >> 5;
+    byte cmdId = packetId & 0b11111;
+    Serial.print("packet with id 0x");
+    Serial.print(packetId, HEX);
+    Serial.print(" Node ");
+    Serial.print(nodeId, HEX);
+    Serial.print(" CMD_ID 0x");
+    Serial.print(cmdId, HEX);
+
+    if (CAN.packetRtr()) {
+      byte dataLength = CAN.packetDlc();
+      Serial.print(" and requested length ");
+      Serial.println(dataLength);
+    } else {
+      Serial.print(" and length ");
+      Serial.println(packetSize);
+      //only print packet data for non-RTR packets
+      // while (CAN.available()) {
+      //   Serial.print(CAN.read(), HEX);
+      // }
+      if (cmdId == GET_ENCODER_ESTIMATES) {
+        byte data[8];
+        byte k = 0;
+        while (CAN.available()) {
+          data[k++] = CAN.read();
+        }
+        Serial.println("\tEncoder Estimates:");
+        dataUnion position, velocity;
+        for (int i = 0; i < 8; i++) {
+          if (i < 4) {
+            position.fBuff[i] = data[i];
+          } else {
+            velocity.fBuff[i-4] = data[i];
+          }
+        }
+        Serial.print("Position:\t");  Serial.println(position.f);
+        Serial.print("Velocity:\t");  Serial.println(velocity.f);
+      }
+    }
+    Serial.println();
+  }
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+"# Arduino-ODrive-CAN-Interface" 
